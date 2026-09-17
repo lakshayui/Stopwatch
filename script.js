7420a0734a0813a3397a04d621811484ff40ee69
lakshayui <lakshaytyagi897@gmail.com>
2026-09-16 22:02:31 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,102 @@
-const display=document.getElementById('countDisplay');
-const decrease=document.getElementById('decreaseBtn');
-const increase=document.getElementById('increaseBtn');
+const display=document.getElementById('timeDisplay');
+const start=document.getElementById('startBtn');
+const stop=document.getElementById('stopBtn');
 const reset=document.getElementById('resetBtn');
 
-let count=0;
+let second=0;
+let timerId=null;
 
-decrease.addEventListener('click',function(){
-    count-=1;
-    display.textContent=count;
+start.addEventListener('click',function(){
+    timerId=setInterval(function(){
+        second+=1;
+        const min=Math.floor(second/60);
+        const sec=second%60;
+        const str=sec.toString();
+        const padstr=str.padStart(2,'0');
+        display.textContent=min+':'+padstr;
+    },1000)
 });
 
-increase.addEventListener('click',function(){
-    count+=1;
-    display.innerText=count;
+stop.addEventListener('click',function(){
+    clearInterval(timerId);
 });
+
 
 reset.addEventListener('click',function(){
-    count=0;
-    display.textContent=count;
+    clearInterval(timerId);
+    second=0;
+    display.textContent='00:00';
 });
 
-display.innerText=count;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 6----padstart(2,"0")--->06   The padStart() method pads a string from the start
+// setInterval -----> The setInterval() method calls a function at specified intervals (in milliseconds)
+
+
+//  const display = document.getElementById('timeDisplay');
+// const start = document.getElementById('startBtn');
+// const stop = document.getElementById('stopBtn');
+// const reset = document.getElementById('resetBtn');
+
+// let second = 0;
+// let timerId = null;
+// start.addEventListener('click', function () {
+//     timerId=setInterval(function(){
+//         second+=1;
+//         const min=Math.floor(second/60);
+//         const sec=second%60;
+//         const string=sec.toString();
+//         const stringval=string.padStart(2,'0');
+//         display.textContent=min+':'+stringval;
+//     },1000)
+
+// });
+
+
+// stop.addEventListener('click',function(){
+//     clearInterval(timerId);
+
+    
+// });
+
+// reset.addEventListener('click', function(){
+//    clearInterval(timerId);
+//    second=0;
+//    display.textContent='00:00';
+// })
